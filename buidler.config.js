f4a9bf0d43606851d81c0346c66fc8acc24aa3f7
7-- <regop412@gmail.com>
2020-10-07 01:15:43 -0400

    change settings for tests

--- buidler.config.js
+++ buidler.config.js
@@ -10,7 +10,7 @@ module.exports = {
   solc: {
     version: "0.6.8",
   },
-  defaultNetwork: "kovan",
+  defaultNetwork: "buidlerevm",
   networks: {
     buidlerevm: {
       allowUnlimitedContractSize: true,
